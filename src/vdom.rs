f7cfd76b31c2f1e00cb633d40753e1861821733a
Anupam Jain <ajnsit@gmail.com>
2022-11-01 03:21:54 +0530

    Simplify update_children

--- src/vdom.rs
+++ src/vdom.rs
@@ -19,6 +19,7 @@ pub(crate) struct VDomMachine<'a>
   }
 
 impl<'a> VDomMachine<'a> {
+
   pub(crate) fn get_node(vdom: &'a VDom<'a, Vec<VDomMachine<'a>>, Node>) -> &Node {
     match vdom {
       VDom::Text { state, .. } => state,
@@ -26,7 +27,7 @@ impl<'a> VDomMachine<'a> {
     }
   }
 
-  pub(crate) fn build_vdom(host: &Host, input: VDom<'a, Vec<VDomTree<'a>>, ()>) -> VDom<'a, Vec<VDomMachine<'a>>, Node> {
+  pub(crate) fn build_vdom(host: &Host, input: &VDom<'a, Vec<VDomTree<'a>>, ()>) -> VDom<'a, Vec<VDomMachine<'a>>, Node> {
     match input {
         VDom::Text { text, .. } => {
           let node = host.create_text_node(text);
@@ -37,32 +38,32 @@ impl<'a> VDomMachine<'a> {
           attrs.iter().for_each(|(key,val)| node.set_attribute(key, val) );
           let children =
             children1.into_iter().map(|child| {
-              let child_vdom = VDomMachine::build_vdom(host, child.vdom);
+              let child_vdom = VDomMachine::build_vdom(host, &child.vdom);
               let child_node = VDomMachine::get_node(&child_vdom);
               // TODO: Insert at the correct index
               node.insert_child_ix(0, child_node);
               VDomMachine { vdom: child_vdom }
           }).collect();
-          VDom::Elem { name, attrs, children, state: node }
+          VDom::Elem { name, attrs: attrs.clone(), children, state: node }
         },
     }
   }
 
-  pub(crate) fn rebuild_dom(&mut self, host: &Host, input: VDom<'a, Vec<VDomTree<'a>>, ()>) {
+  pub(crate) fn rebuild_dom(&mut self, host: &Host, input: &VDom<'a, Vec<VDomTree<'a>>, ()>) {
     self.vdom = Self::build_vdom(host, input);
   }
 
-  pub(crate) fn halt_and_rebuild(&mut self, host: &Host, input: VDom<'a, Vec<VDomTree<'a>>, ()>) {
+  pub(crate) fn halt_and_rebuild(&mut self, host: &Host, input: &VDom<'a, Vec<VDomTree<'a>>, ()>) {
     self.halt();
     self.rebuild_dom(host, input);
   }
 
-  pub(crate) fn step(&mut self, host: &Host, input: VDom<'a, Vec<VDomTree<'a>>, ()>) {
+  pub(crate) fn step(&mut self, host: &Host, input: &VDom<'a, Vec<VDomTree<'a>>, ()>) {
     match input {
       VDom::Text { text: tnew, .. } => {
         match &mut self.vdom {
           VDom::Text { text: told, state: node } =>
-            if tnew != *told {
+            if tnew != told {
               node.set_text_content(tnew);
               *told = tnew;
             },
@@ -72,23 +73,20 @@ impl<'a> VDomMachine<'a> {
         }
       },
       VDom::Elem { name: name_new, attrs: attrs_new, children: children_new, .. } => {
-        let mut vdom_temp = VDomMachine {vdom: VDom::Text { text: "DELME", state: Node::dummyNode() } };
-        std::mem::swap(self, &mut vdom_temp);
-        match vdom_temp.vdom {
-          VDom::Elem { name: name_old, attrs: attrs_old, children: children_old, state: mut node } => {
-            if name_new == name_old {
+        match std::mem::replace(&mut self.vdom, VDom::Text { text: "DELME", state: Node::dummyNode() }) {
+          VDom::Elem { name: name_old, attrs: attrs_old, children: mut children_old, state: mut node } => {
+            if *name_new == name_old {
               update_attrs(&mut node, &attrs_old, &attrs_new);
               if children_old.len() != 0 || children_new.len() != 0 {
-                let new_children = update_children(host, &mut node, children_old, children_new);
-                vdom_temp.vdom = VDom::Elem { name: name_old, attrs: attrs_new, children: new_children, state: node };
-                std::mem::swap(self, &mut vdom_temp);
+                update_children(host, &mut node, &mut children_old, children_new);
+                self.vdom = VDom::Elem { name: name_old, attrs: attrs_new.clone(), children: children_old, state: node };
               }
             } else {
-              self.rebuild_dom(host, VDom::Elem { name: name_old, attrs: attrs_new, children: children_new, state: () });
+              self.halt_and_rebuild(host, input);
             }
           },
-          VDom::Text { text, .. } => {
-            self.rebuild_dom(host, VDom::Text { text, state: () });
+          VDom::Text { .. } => {
+            self.halt_and_rebuild(host, input);
           },
         }
       },
@@ -96,7 +94,7 @@ impl<'a> VDomMachine<'a> {
   }
 
 
-  pub(crate) fn halt(&mut self) {
+  pub(crate) fn halt(&self) {
     match &mut self.vdom {
       VDom::Text { state: node , .. } => {
         let parent = node.parent_node();
@@ -128,37 +126,19 @@ fn update_attrs(node: &mut Node, attrs_old: &Attrs, attrs_new: &Attrs) {
   });
 }
 
-fn update_children<'a>(host: &Host, parent: &mut Node, children_old: Vec<VDomMachine<'a>>, children_new: Vec<VDomTree<'a>>) -> Vec<VDomMachine<'a>> {
-  // TODO: Currently only unkeyed is supported as a POC
-  let mut ret = Vec::new();
-  let mut children_old_iter = children_old.into_iter();
-  let mut children_new_iter = children_new.into_iter();
-  loop {
-    match children_old_iter.next() {
-      Some(mut child_old) => {
-        match children_new_iter.next() {
-          Some(child_new) => {
-            child_old.step(host, child_new.vdom);
-            ret.push(child_old);
-          }
-          None => {
-            child_old.halt();
-          },
-        }
-      }
-      None => {
-        match children_new_iter.next() {
-          Some(child_new) => {
-            let child_vdom = VDomMachine::build_vdom(host, child_new.vdom);
-            let child_node = VDomMachine::get_node(&child_vdom);
-            // TODO: Insert at the correct index
-            parent.insert_child_ix(0, child_node);
-            ret.push(VDomMachine { vdom: child_vdom });
-          }
-          None => break,
-        }
-      }
-    }
+fn update_children<'a>(host: &Host, parent: &mut Node, children_old: &mut Vec<VDomMachine<'a>>, children_new: &Vec<VDomTree<'a>>) {
+  for(child_old, child_new) in children_old.iter_mut().zip(children_new) {
+    child_old.step(host, &child_new.vdom);
   }
-  ret
+  let to_be_removed = children_old.drain(children_new.len()..);
+  for child in to_be_removed {
+    child.halt();
+  }
+  for child_new in children_new.iter().skip(children_old.len()) {
+    let child_vdom = VDomMachine::build_vdom(host, &child_new.vdom);
+    let child_node = VDomMachine::get_node(&child_vdom);
+    // TODO: Insert at the correct index
+    parent.insert_child_ix(0, child_node); 
+    children_old.push(VDomMachine { vdom: child_vdom });
+  }
 }
