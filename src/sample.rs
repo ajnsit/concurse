e56cafc041aefb273e057ac7c3cd82578f579360
Anupam Jain <ajnsit@gmail.com>
2022-12-01 21:20:55 +0530

    Demonstrate many counters!
    We don't yet have `orr` or any concurrent widgets, so these are not 1000
    independent counter widgets, but only one widget where the view shows
    1000 buttons instead of a single one.

--- src/sample.rs
+++ src/sample.rs
@@ -36,7 +36,7 @@ pub(crate) fn test(name: &str) -> Result<(), JsValue> {
     let marc1 = Arc::from(Mutex::from(machine));
     let marc2 = marc1.clone();
 
-    let vdom = counter(href, marc1, 0);
+    let vdom = many_counter(1000, href, marc1, 0);
 
     take_mut::take(
         marc2
@@ -50,22 +50,37 @@ pub(crate) fn test(name: &str) -> Result<(), JsValue> {
     Ok(())
 }
 
-fn counter(href: &'static Host, marc1: Arc<Mutex<VDom<Node>>>, count: i32) -> VDom<()> {
-    let marc2 = marc1.clone();
-    let marc3 = marc1.clone();
-    let listener = Listener {
-        handler: Closure::once(move || {
-            take_mut::take(
-                marc2
-                    .as_ref()
-                    .lock()
-                    .expect("Failed to lock machine within a handler")
-                    .deref_mut(),
-                |m| step(m, href, counter(href, marc3, count + 1)),
-            );
-            log!("CLICKED!");
+fn many_counter(n: u32, href: &'static Host, marc: Arc<Mutex<VDom<Node>>>, count: i32) -> VDom<()> {
+    let mut counters = Vec::new();
+    for _ in 0..n {
+        let marc1 = marc.clone();
+        let marc2 = marc.clone();
+        let listener = Listener {
+            handler: Closure::once(move || {
+                take_mut::take(
+                    marc1
+                        .as_ref()
+                        .lock()
+                        .expect("Failed to lock machine within a handler")
+                        .deref_mut(),
+                    |m| step(m, href, many_counter(n, href, marc2, count + 1)),
+                );
+                log!("CLICKED!");
+            }),
+        };
+        counters.push(counter(count, listener));
+    }
+    VDom {
+        vdom: VDomNode::Elem(Elem {
+            name: "div".to_owned(),
+            attrs: HashMap::default(),
+            children: Vec::from(counters),
+            state: (),
         }),
-    };
+    }
+}
+
+fn counter(count: i32, listener: Listener) -> VDom<()> {
     VDom {
         vdom: VDomNode::Elem(Elem {
             name: "div".to_owned(),
